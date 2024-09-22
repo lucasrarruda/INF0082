c2295c57e170320e90117784b27f82652450250e
Lucas Arruda <lucasrarruda@gmail.com>
2024-09-22 16:57:08 -0300

    Added Ex3 done

--- Ex3/Ex3.cs
+++ Ex3/Ex3.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
 
         // Obter o número de pedidos (P) e de cozinheiros (C)
@@ -23,5 +23,71 @@ class Program
         }
 
         // Continue a implementação
+        List<Chef> chefs = new();
+        for(int i = 0; i < C; i++)
+        {
+            chefs.Add(new Chef());
+        }
+
+        Manager manager = new(chefs);
+        List<Task> waitingOrder = new();
+
+        foreach(var orderRequested in pedidos)
+        {
+            Order order = new()
+            {
+                Name = orderRequested.prato,
+                PreparationTime = orderRequested.tempo
+            };
+            waitingOrder.Add(manager.RequestOrder(order));
+        }
+
+        await Task.WhenAll(waitingOrder);
+    }
+
+    public class Order
+    {
+        public required string Name { get; set; }
+        public required int PreparationTime { get; set; }
+    }
+
+    public class Chef
+    {
+        private SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
+
+        public async Task Cook(Order order)
+        {
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                Console.WriteLine($"O prato '{order.Name}' começou a ser preparado.");
+                await Task.Delay(order.PreparationTime);
+                Console.WriteLine($"O prato '{order.Name}' está pronto.");
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
+        }
+    }
+
+    public class Manager
+    {
+        private List<Chef> _chefs;
+        private int _chefRound = 0;
+
+        public Manager(List<Chef> chefs)
+        {
+            _chefs = chefs;
+        }
+
+        public async Task RequestOrder(Order order)
+        {
+            Chef selectedChef = _chefs[_chefRound];
+            
+            _chefRound = (_chefRound + 1) % _chefs.Count;   
+
+            await selectedChef.Cook(order);
+        }
     }
 }
