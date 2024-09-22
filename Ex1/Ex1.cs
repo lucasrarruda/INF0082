03007947cd4b8525eb0fa5bd4f9346f5d09abf34
Lucas Arruda <lucasrarruda@gmail.com>
2024-09-22 14:27:27 -0300

    Added Ex1 done

--- Ex1/Ex1.cs
+++ Ex1/Ex1.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         // Leitura do número de regiões
         int R = int.Parse(Console.ReadLine());
@@ -21,6 +22,46 @@ class Program
         }
 
         // Continue a implementação
+        List<Task> regions = new List<Task>();
+        VotingMachine voteMachine = new();
+        Random random = new();
 
+        foreach(var currentVotes in votos)
+        {
+            regions.Add(Task.Run(() => 
+            {
+                int delay = random.Next(500, 2000);
+                Task.Delay(delay);
+                voteMachine.AddVotes("A", currentVotes.votosChapaA);
+                Task.Delay(delay);
+                voteMachine.AddVotes("B", currentVotes.votosChapaB);
+            }));
+        }
+
+        await Task.WhenAll(regions);
+        voteMachine.ShowWinner();
+    }
+
+    public class VotingMachine
+    {
+        private ConcurrentDictionary<string, int> _voting = new ConcurrentDictionary<string, int>();
+
+        public void AddVotes(string politicalParty, int votes)
+        {
+            _voting.AddOrUpdate(politicalParty, votes, (key, currentValue) => currentValue + votes);
+        }
+
+        public void ShowWinner()
+        {
+            int votingA = 0;
+            int votingB = 0;
+            _voting.TryGetValue("A", out votingA);
+            _voting.TryGetValue("B", out votingB);
+            
+            Console.WriteLine($"Chapa A: {votingA} Votos");
+            Console.WriteLine($"Chapa B: {votingB} Votos");
+            string winner = votingA >= votingB ? "A": "B";
+            Console.WriteLine($"A chapa {winner} venceu a eleição!");
+        }
     }
 }
